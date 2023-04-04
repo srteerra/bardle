f4a4e3ae6b8ba6552c15166590f6a4178d1db875
Angel Lopez <srterradev@gmail.com>
2023-04-04 15:58:40 -0600

    feat: pushing progress

---
+++ bardle/src/pages/_document.js
@@ -0,0 +1,18 @@
+"use client";
+import { Html, Head, Main, NextScript } from "next/document";
+import { ThemeProvider } from "next-themes";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+
+      <body className="dark:bg-stone-800 bg-green-50">
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Main />
+          <NextScript />
+        </ThemeProvider>
+      </body>
+    </Html>
+  );
+}
