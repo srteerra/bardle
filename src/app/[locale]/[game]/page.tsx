7b5ef7846dec29ec0d75beb3ea6dbd11977d7e4f
Angel Lopez <srterradev@gmail.com>
2024-07-23 00:13:24 -0600

    chore: daily and rush mode layouts

--- src/app/[locale]/[game]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from "react";
-
-const GamePage = () => {
-  return <div>Game Page</div>;
-};
-
-export default GamePage;
---
+++ src/app/[locale]/daily/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const DailyGameMode = () => {
+  return <div>Daily Page</div>;
+};
+
+export default DailyGameMode;
