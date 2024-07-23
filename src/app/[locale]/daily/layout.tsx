4f4455452b0544fb5fa20a774d36b9f6b53663a3
Angel Lopez <srterradev@gmail.com>
2024-07-23 00:12:57 -0600

    GameMode Layouts (#19)
    * feat: middleware config changed
    
    * feat: middleware config for routes without locale
    
    * feat: middleware config for routes without locale
    
    * chore: daily and rush mode layouts

---
+++ src/app/[locale]/daily/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import React from "react";
+import "../../globals.css";
+import { NextIntlClientProvider, useMessages } from "next-intl";
+
+export const metadata: Metadata = {
+  title: "Bardle",
+  description: "Bardle Official",
+};
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const messages = useMessages();
+  console.log(locale);
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/rush/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const RushGameMode = () => {
+  return <div>Rush Page</div>;
+};
+
+export default RushGameMode;
