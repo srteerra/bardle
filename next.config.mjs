721e7b3795c1828db742d042650d7f928d2180a9
Angel Lopez <srterradev@gmail.com>
2024-07-29 22:48:11 -0600

    Guessing Row component (#25)
    * faet: daily and guessRows components
    
    * faet: daily and guessRows components
    
    * feat: GuessingRow for daily layout
    
    * feat: GuessingRow for daily layout

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: false,
+    domains: ["cdn.communitydragon.org", "ddragon.leagueoflegends.com"],
+  },
+};
 
 export default withNextIntl(nextConfig);
---
+++ src/interfaces/IChampion.ts
@@ -0,0 +1,14 @@
+export interface IChampion {
+  displayName: string;
+  shortName: string;
+  gender: number;
+  partype: number;
+  position: string[];
+  rangeType: string[];
+  regionFrom: string[];
+  releaseDate: string;
+  role: string[];
+  specie: string[];
+  titleChamp: string;
+  avatarURL?: string;
+}
